a006a8aefc4d322e9c6a8cbcc4671e4a69dfb592
Radoslav Nikolov <124899904+rado8802@users.noreply.github.com>
2025-11-15 21:28:08 +0200

    Add TestData class with email and password constants

---
+++ Util/TestData.cs
@@ -0,0 +1,8 @@
+namespace AutomationExerciseTests.Util
+{
+    public static class TestData
+    {
+        public const string ValidEmail = "fena7vd@gmail.com";   // смени с реални, ако имаш
+        public const string ValidPassword = "1234567890";
+    }
+}
