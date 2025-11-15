f65790a56af5cf1c748426d6e956fc5c5339bec3
Radoslav Nikolov <124899904+rado8802@users.noreply.github.com>
2025-11-15 21:13:31 +0200

    Add CheckoutPage class for checkout functionality

---
+++ Pages/CheckoutPage.cs
@@ -0,0 +1,17 @@
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace AutomationExerciseTests.Pages
+{
+    public class CheckoutPage : BasePage
+    {
+        public CheckoutPage(IPage page) : base(page) { }
+
+        public ILocator PlaceOrderButton => Page.GetByRole(AriaRole.Link, new() { Name = "Place Order" });
+
+        public async Task OpenAsync()
+        {
+            await Page.GotoAsync("https://automationexercise.com/checkout");
+        }
+    }
+}
